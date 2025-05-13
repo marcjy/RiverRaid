e16d7080897dde3dc49462942f174397d155e772
Marc <marcmartin315@gmail.com>
2025-05-13 15:04:49 +0200

    Added event that will trigger when the player pressed the play again button in the end game window. Added event that will trigger when the player pressed the quit button in the end game window.

--- RiverRaid/Assets/Scripts/UI/UIEvents.cs
+++ RiverRaid/Assets/Scripts/UI/UIEvents.cs
@@ -2,7 +2,16 @@ using System;
 
 public static class UIEvents
 {
+    // UIStartGame
     public static event EventHandler OnStartGameAnimationCompleted;
 
     public static void TriggerIntroAnimaitonCompleted() => OnStartGameAnimationCompleted?.Invoke(null, EventArgs.Empty);
+
+    //UIEndGame
+    public static event EventHandler OnPlayAgain;
+    public static event EventHandler OnQuit;
+
+    public static void TriggerPlayAgain() => OnPlayAgain?.Invoke(null, EventArgs.Empty);
+    public static void TriggerQuit() => OnQuit?.Invoke(null, EventArgs.Empty);
+
 }
