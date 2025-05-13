197db94c84e3449c04be7cd7f5436fc104406d35
Marc <marcmartin315@gmail.com>
2025-05-13 12:49:27 +0200

    Created script that will make a spirte start blinking for a determined duration.

---
+++ RiverRaid/Assets/Scripts/Player/BlinkingSprite.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+
+public class BlinkingSprite : MonoBehaviour
+{
+    public float BlinkDuration;
+    public float TimeBetweenStates;
+
+    private SpriteRenderer _renderer;
+
+    private void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+
+        GameManager.Instance.OnResetLevel += HandleResetLevel;
+    }
+
+    private void HandleResetLevel(object sender, System.EventArgs e) => StartCoroutine(StartBlinking());
+
+    private IEnumerator StartBlinking()
+    {
+        float elapsedTime = 0.0f;
+        bool isVisible = true;
+
+        while (elapsedTime < BlinkDuration)
+        {
+            _renderer.enabled = isVisible;
+            isVisible = !isVisible;
+
+            yield return new WaitForSeconds(TimeBetweenStates);
+
+            elapsedTime += Time.deltaTime;
+        }
+
+        _renderer.enabled = true;
+    }
+}
