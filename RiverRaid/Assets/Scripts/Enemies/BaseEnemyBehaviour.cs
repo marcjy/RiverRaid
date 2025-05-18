45a1910c6dc249a034bf915ea268b50df46cdfef
Marc <marcmartin315@gmail.com>
2025-05-18 13:41:45 +0200

    Created enemy behaviour where the enemy will move side to side in  the X-axis, taking into account the bounds of the river. This behaviour does not work yet when the river is split into two rivers.

--- RiverRaid/Assets/Scripts/Enemies/BaseEnemyBehaviour.cs
+++ RiverRaid/Assets/Scripts/Enemies/BaseEnemyBehaviour.cs
@@ -5,18 +5,26 @@ public abstract class BaseEnemyBehaviour : MonoBehaviour, IGenerable
 {
     public event EventHandler OnShouldBeReleased;
 
+    protected float _minPositionY;
+    protected int _spawnPositionY;
+
     protected EnemyDestructible _enemyDestructible;
 
-    private void Start()
+    protected virtual void Awake()
     {
         _enemyDestructible = GetComponent<EnemyDestructible>();
         _enemyDestructible.OnDestroyed += HandleDestroyed;
+
+        _spawnPositionY = Mathf.CeilToInt(Camera.main.transform.position.y + Camera.main.orthographicSize);
+        _minPositionY = FindAnyObjectByType<PlayerController>().gameObject.transform.position.y - 1;
     }
 
     public abstract void Init();
 
+    protected void TriggerOnShouldBeReleased() => OnShouldBeReleased?.Invoke(this, EventArgs.Empty);
+
     #region Event Handling
-    private void HandleDestroyed(object sender, int e) => OnShouldBeReleased?.Invoke(this, EventArgs.Empty);
+    private void HandleDestroyed(object sender, int e) => TriggerOnShouldBeReleased();
     #endregion
 
 }
---
+++ RiverRaid/Assets/Scripts/Enemies/MoveSideToSideEnemyBehaviour.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+
+[RequireComponent(typeof(ScrollVertically))]
+public class MoveSideToSideEnemyBehaviour : BaseEnemyBehaviour
+{
+    public float Speed;
+    public Sprite[] PossibleSprites;
+
+    private LevelManager _levelManager;
+    private RiverManager _riverManager;
+    private SpriteRenderer _spriteRenderer;
+    private ScrollVertically _scrollVertically;
+
+    private int _minPositionX;
+    private int _maxPositionX;
+
+    private float _targetX;
+    private float _lastYUsedForXBounds;
+    private bool _movingLeft;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _levelManager = FindAnyObjectByType<LevelManager>();
+        _lastYUsedForXBounds = _spawnPositionY;
+
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.sprite = PossibleSprites[Random.Range(0, PossibleSprites.Length)];
+
+        _scrollVertically = GetComponent<ScrollVertically>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        if (transform.position.y <= _minPositionY)
+            TriggerOnShouldBeReleased();
+        else
+            if (HasYChangedSinceLastBoundsCheck())
+        {
+            CalculateXBoundsAtY(Mathf.FloorToInt(transform.position.y));
+            _lastYUsedForXBounds = transform.position.y;
+        }
+    }
+
+    public override void Init()
+    {
+        _riverManager = _levelManager.CurrentLevel.GetComponentInChildren<RiverManager>();
+        CalculateXBoundsAtY(_spawnPositionY);
+
+        transform.position = new Vector3(Random.Range(_minPositionX, _maxPositionX + 1), _spawnPositionY, 0);
+
+        _movingLeft = Random.Range(0, 2) == 0;
+        _targetX = _movingLeft ? _minPositionX : _maxPositionX;
+        transform.rotation = _movingLeft ? Quaternion.Euler(0, 0, 90) : Quaternion.Euler(0, 0, -90);
+
+        _scrollVertically.RecalculateSpeed();
+    }
+
+    private void Move()
+    {
+        Vector3 currentPosition = transform.position;
+
+        transform.position = Vector3.MoveTowards(currentPosition, new Vector3(_targetX, currentPosition.y, currentPosition.z), Speed * Time.deltaTime);
+
+        if (Mathf.Approximately(transform.position.x, _targetX))
+        {
+            _movingLeft = !_movingLeft;
+            FlipEnemy();
+            _targetX = _movingLeft ? _minPositionX : _maxPositionX;
+        }
+    }
+
+    private void CalculateXBoundsAtY(int y)
+    {
+        _riverManager.GetXBoundsGivenY(y, out _minPositionX, out _maxPositionX);
+
+        _targetX = _movingLeft ? _minPositionX : _maxPositionX;
+    }
+    private bool HasYChangedSinceLastBoundsCheck()
+    {
+        int previousY = Mathf.FloorToInt(_lastYUsedForXBounds);
+        int currentY = Mathf.FloorToInt(transform.position.y);
+
+        return previousY != currentY;
+    }
+
+    private void FlipEnemy() => transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z * -1);
+}
