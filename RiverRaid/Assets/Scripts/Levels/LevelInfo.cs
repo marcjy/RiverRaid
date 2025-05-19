dc6f4c6afab267157878dd26d7aacd38b36fabb9
Marc <marcmartin315@gmail.com>
2025-05-19 12:43:27 +0200

    LevelInfo now tracks how much the level has scrolled vertically, exposing the amount of units scrolled in TrackeablePositionY.

--- RiverRaid/Assets/Scripts/Levels/LevelInfo.cs
+++ RiverRaid/Assets/Scripts/Levels/LevelInfo.cs
@@ -5,12 +5,31 @@ public class LevelInfo : MonoBehaviour
 {
     public Tilemap BridgeTilemap;
     public float BridgePositionY { get; private set; }
+    public float TrackeablePositionY
+    {
+        get
+        {
+            return _trackeablePositionY % BridgePositionY;
+        }
 
+        private set => _trackeablePositionY = value;
+    }
+
+    private ScrollVertically _scrollVertically;
+    private float _trackeablePositionY;
     private const int OFFSET_Y = 6;
 
     private void Awake()
     {
         BoundsInt bounds = BridgeTilemap.cellBounds;
         BridgePositionY = bounds.yMax + OFFSET_Y;
+
+        _trackeablePositionY = 0;
+        _scrollVertically = GetComponent<ScrollVertically>();
+    }
+
+    private void Update()
+    {
+        _trackeablePositionY += _scrollVertically.CurrentSpeed * Time.deltaTime;
     }
 }
