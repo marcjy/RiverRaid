4435395e633dc0e76d33fea3c6c3b55ade48e741
Marc <marcmartin315@gmail.com>
2025-05-28 11:29:31 +0200

    Created script that will enable static classes to run coroutines.

---
+++ RiverRaid/Assets/Scripts/Utils/CoroutineRunner.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CoroutineRunner : MonoBehaviour
+{
+    private static CoroutineRunner _instance;
+
+    public static CoroutineRunner Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject runner = new GameObject("CoroutineRunner");
+                _instance = runner.AddComponent<CoroutineRunner>();
+            }
+            return _instance;
+        }
+    }
+}
