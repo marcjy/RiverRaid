57dc798e7383676170d86d1ff6f6dd87a48317b9
Marc <marcmartin315@gmail.com>
2025-06-10 15:54:01 +0200

    PlayerCollisionDetector now ignores all collisions for a determined time when the player died recently.

--- RiverRaid/Assets/Scripts/Player/PlayerCollisionDetector.cs
+++ RiverRaid/Assets/Scripts/Player/PlayerCollisionDetector.cs
@@ -5,10 +5,32 @@ public class PlayerCollisionDetector : MonoBehaviour
 {
     public event EventHandler OnHazardCollision;
 
+    private readonly float _ignoreCollisionsDuration = 2.0f;
+    private float _ignoreCollisionsCounter = 0.0f;
+    private bool _isIgnoringCollisions = false;
+
+    private void Update()
+    {
+        if (_isIgnoringCollisions)
+            _ignoreCollisionsCounter += Time.deltaTime;
+
+        if (_ignoreCollisionsCounter >= _ignoreCollisionsDuration)
+        {
+            _ignoreCollisionsCounter = 0.0f;
+            _isIgnoringCollisions = false;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (_isIgnoringCollisions)
+            return;
+
         if (collision.gameObject.CompareTag("Hazard"))
+        {
             OnHazardCollision?.Invoke(this, EventArgs.Empty);
+            _isIgnoringCollisions = true;
+        }
 
         if (collision.TryGetComponent(out ICollectable collectable))
             collectable.Collect(gameObject);
